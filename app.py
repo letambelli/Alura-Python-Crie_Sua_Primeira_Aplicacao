10d0c1364e88fade9b185ab0f78c014232681cfe
Letícia Tambelli <leticiatambelli2@gmail.com>
2025-05-10 10:33:08 -0300

    primeiro commit

---
+++ app.py
@@ -0,0 +1,30 @@
+print("""
+╭━━━╮╱╱╭╮╱╱╱╱╱╱╱╭━━━╮
+┃╭━╮┃╱╱┃┃╱╱╱╱╱╱╱┃╭━━╯
+┃╰━━┳━━┫╰━┳━━┳━╮┃╰━━┳╮╭┳━━┳━┳━━┳━━┳━━╮
+╰━━╮┃╭╮┃╭╮┃╭╮┃╭╯┃╭━━┻╋╋┫╭╮┃╭┫┃━┫━━┫━━┫
+┃╰━╯┃╭╮┃╰╯┃╰╯┃┃╱┃╰━━┳╋╋┫╰╯┃┃┃┃━╋━━┣━━┃
+╰━━━┻╯╰┻━━┻━━┻╯╱╰━━━┻╯╰┫╭━┻╯╰━━┻━━┻━━╯
+╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱┃┃
+╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╰╯
+      """)
+
+print('1. Cadastrar Restaurante')
+print('2. Listar Restaurantes')
+print('3. Ativar Restaurante')
+print('4. Sair\n')
+
+opcao_escolhida = int(input('Escolha uma opção: '))
+
+print(f'Você escolheu a opção {opcao_escolhida}')
+
+if opcao_escolhida == 1:
+    print('Cadastrar Restaurante')
+elif opcao_escolhida == 2:
+    print('Listar Restaurantes')
+elif opcao_escolhida == 3:
+    print('Ativar Restaurante')
+elif opcao_escolhida == 4:
+    print('Sair')
+else:
+    print('ERRO')
