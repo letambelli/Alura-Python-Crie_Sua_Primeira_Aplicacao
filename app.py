1e357ccf73eb0718df282555a120d75226645ad1
Letícia Tambelli <leticiatambelli2@gmail.com>
2025-05-13 14:54:31 -0300

    listar restaurantes adicionado

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-restaurantes = []
+restaurantes = ['Pizza', 'Sushi']
 
 def exibir_o_nome_do_programa():
       print("""
@@ -34,7 +34,15 @@ def cadatrar_novo_restaurante():
       nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
       restaurantes.append(nome_do_restaurante)
       print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
-      input('Digite uma tecla para voltar ao menu principal')
+      input('\nDigite uma tecla para voltar ao menu principal')
+      main()
+
+def listar_restaurantes():
+      os.system('cls')
+      print('Listando Restaurantesz\n')
+      for restaurante in restaurantes:
+            print(f'{restaurante}')
+      input('\nDigite uma tecla para voltar ao menu principal')
       main()
 
 def escolher_opcao():
@@ -46,7 +54,7 @@ def escolher_opcao():
                   case 1:
                         cadatrar_novo_restaurante()
                   case 2:
-                        print('Listar Restaurantes')
+                        listar_restaurantes()
                   case 3:
                         print('Ativar Restaurante')
                   case 4:
