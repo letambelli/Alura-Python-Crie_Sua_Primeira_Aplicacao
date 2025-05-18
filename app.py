0ff27c601aab9cd9b67e0dec334e94db772abf99
Letícia Tambelli <leticiatambelli2@gmail.com>
2025-05-18 00:35:36 -0300

    ativacao restaurantes

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def cadatrar_novo_restaurante():
       nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
       categoria = input(f'Digite o nome da categoria do restaurante {nome_do_restaurante}: ')
       dados_do_restaurante = {'nome': nome_do_restaurante, 'categoria': categoria, 'ativo': False}
-      restaurantes.append(dados_do_restaurante )
+      restaurantes.append(dados_do_restaurante)
       print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!\n')
       voltar_ao_menu_principal()
 
@@ -54,6 +54,24 @@ def listar_restaurantes():
             categoria = restaurante['categoria']
             ativo = restaurante['ativo']
             print(f'- {nome_restaurante} | {categoria} | {ativo}')
+      voltar_ao_menu_principal()
+
+def alternar_estado_restaurante():
+      exibir_subtitulo('Alternando o estado do restaurante')
+      nome_restaurante = input('Digite o nome do restaurante que deseja alternar o estado: ')
+      restaurante_encontrado = False
+
+      for restaurante in restaurantes:
+            if nome_restaurante == restaurante['nome']:
+                  restaurante_encontrado = True
+                  restaurante['ativo'] = not restaurante['ativo']
+                  mensagem = f'O restaurante {nome_restaurante} foi ativado com sucesso' if restaurante['ativo'] else f'O restaurante {nome_restaurante} foi desativado com sucesso'
+                  print(mensagem)
+      if not restaurante_encontrado:
+            print('O restaurante não foi encontrado')
+
+
+
       voltar_ao_menu_principal()
 
 def escolher_opcao():
@@ -67,10 +85,9 @@ def escolher_opcao():
                   case 2:
                         listar_restaurantes()
                   case 3:
-                        print('Ativar Restaurante')
+                        alternar_estado_restaurante()
                   case 4:
                         finalizar_app()
-                        print('Finalizando app\n')
                   case _:
                         opcao_invalida()
       except:
